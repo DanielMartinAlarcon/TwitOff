5303df9a1001529b07f863e9ddfd2ffdbeac7c04
DanielMartinAlarcon <daniel@martinalarcon.org>
2019-03-05 10:09:47 -0800

    Mar 5 initial commit

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,5 +1,5 @@
 """Main application and routing logic for TwitOff."""
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from .models import DB, User
 
 
@@ -7,11 +7,14 @@ def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     DB.init_app(app)
 
     @app.route('/')
     def root():
-        return render_template('home.html')
+        users = User.query.all()
+        return render_template('base.html', 
+                title='Home', users=users)
 
     @app.route('/users')
     def users():
