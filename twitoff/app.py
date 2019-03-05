cc9f6bae2afcc7136849013469b28f9a8a9c665c
DanielMartinAlarcon <daniel@martinalarcon.org>
2019-03-05 14:55:13 -0800

    Successful connection to twitter and basilica APIs

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,4 +1,5 @@
 """Main application and routing logic for TwitOff."""
+from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
 
@@ -6,20 +7,20 @@ from .models import DB, User
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['ENV'] = config('FLASK_ENV')
     DB.init_app(app)
 
     @app.route('/')
     def root():
         users = User.query.all()
-        return render_template('base.html', 
-                title='Home', users=users)
+        return render_template('base.html', title='Home', users=users)
 
-    @app.route('/users')
-    def users():
-        all_users = User.query.all()
-        return render_template('users.html', 
-                                all_users=all_users)
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='DB Reset!', users=[])
 
     return app
