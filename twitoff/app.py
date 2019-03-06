2b872e2e661e103dd918f27e6bf47fe2a9494fb6
DanielMartinAlarcon <daniel@martinalarcon.org>
2019-03-06 15:32:15 -0800

    EOD Mar 6

--- twitoff/app.py
+++ twitoff/app.py
@@ -2,20 +2,20 @@
 from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
-
+from .twitter import add_or_update_user
+from .predict import predict_user
 
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['ENV'] = config('FLASK_ENV')
     DB.init_app(app)
 
     @app.route('/')
     def root():
         users = User.query.all()
-        return render_template('home.html', users=users)
+        return render_template('base.html', users=users)
 
     @app.route('/reset')
     def reset():
@@ -23,10 +23,37 @@ def create_app():
         DB.create_all()
         return render_template('reset.html')
 
-    @app.route('/user/<name>')
+
+    @app.route('/compare', methods=['POST'])
+    def compare():
+        user1, user2 = request.values['user1'], request.values['user2']
+        if user1 == user2:
+            return 'Cannot compare a user to themselves!'
+        else:
+            prediction = predict_user(user1, user2,
+                                      request.values['tweet_text'])
+            # return user1 if prediction else user2
+        return render_template('compare.html', 
+                            title='User comparison',
+                            user_1=user1,
+                            user_2=user2,
+                            prob_user_1=f'{prediction[0][0]*100:0.0f}',
+                            prob_user_2=f'{prediction[0][1]*100:0.0f}')
+
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
     def user(name=None):
-        user = User.query.filter_by(name=name).first()
-        return render_template('user.html', 
-                                user=user)
+        message=''
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = f'User {name} successfully added!'
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = f'Error adding {name}: {e}'
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets,
+                            message=message)
 
     return app
