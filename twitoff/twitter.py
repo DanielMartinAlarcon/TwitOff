7b1550cd6a7b16f109631b73dba81a93e089f40e
DanielMartinAlarcon <daniel@martinalarcon.org>
2019-03-05 16:49:08 -0800

    Links to user tweets!

--- twitoff/twitter.py
+++ twitoff/twitter.py
@@ -14,5 +14,23 @@ TWITTER = tweepy.API(TWITTER_AUTH)
 
 BASILICA = basilica.Connection(config('BASILICA_KEY'))
 
+def add_user(handle):
+    twitter_user = TWITTER.get_user(handle)
+    tweets = twitter_user.timeline(count=200, 
+                                    exclude_replies=True, 
+                                    include_rts=False, 
+                                    tweet_mode='extended')
+    db_user = User(id=twitter_user.id, 
+                    name=twitter_user.screen_name, 
+                    newest_tweet_id=tweets[0].id)
+    
+    for tweet in tweets:
+        embedding = BASILICA.embed_sentence(tweet.full_text, model='twitter')
+        db_tweet = Tweet(id=tweet.id, 
+                        text=tweet.full_text[:500], 
+                        embedding=embedding)
+        DB.session.add(db_tweet)
+        db_user.tweets.append(db_tweet)
 
-# TODO - Write some useful functions!
+    DB.session.add(db_user)
+    DB.session.commit()
